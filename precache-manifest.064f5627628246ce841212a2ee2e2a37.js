ecabd7b048f49c3b5f0bdea44e43b458d80f68b8
Riaz Laskar <riazcool77@gmail.com>
2020-06-25 16:29:09 +0530

    Updates

---
+++ precache-manifest.064f5627628246ce841212a2ee2e2a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23816096be59657593797a52f0c8722",
+    "url": "./index.html"
+  },
+  {
+    "revision": "355caf9fc8a19eca4a06",
+    "url": "./static/css/main.fb5c7a45.chunk.css"
+  },
+  {
+    "revision": "01af8666057ba334767e",
+    "url": "./static/js/2.fd3e5729.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fd3e5729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355caf9fc8a19eca4a06",
+    "url": "./static/js/main.a6abefb8.chunk.js"
+  },
+  {
+    "revision": "14ff2d3df94723f3c7b6",
+    "url": "./static/js/runtime-main.348d0116.js"
+  }
+]);
